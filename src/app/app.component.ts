6f390a270500e3fd7be9283f3de495d9a0bc531a
saiprasadbsc <saiprasadbsc8@gmail.com>
2021-07-20 13:39:33 +0530

    new changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Recharge & Bill Pay', url: '/rechargebills', icon: 'battery-charging-sharp' },
-    { title: 'Transfer to Bank', url: '/banktransfer', icon: 'dth' },
-    { title: 'Offers & Deals', url: '/offers', icon: 'dth' },
-    { title: 'Boost-Loans', url: '/boostloan', icon: 'dth' },
-    { title: 'Local-Stores', url: '/localstores', icon: 'dth' },
-    { title: 'Redeem-Pay back points', url: '/redeem', icon: 'dth' },
-    { title: 'Reedem-MGM Points', url: '/redeemmgmpoints', icon: 'dth' },
-    { title: 'Wallet-Transfer', url: '/wallet', icon: 'dth' },
+    { title: 'Recharge & Bill Pay', url: '/rechargebills', image: '../assets/icon/recharge.png'},
+    { title: 'Transfer to Bank', url: '/banktransfer', image:'../assets/icon/bank.png' },
+    { title: 'Offers & Deals', url: '/offers', image:'../assets/icon/offers.png' },
+    { title: 'Boost-Loans', url: '/boostloan', image:'../assets/icon/loan.png' },
+    { title: 'Local-Stores', url: '/localstores', image:'../assets/icon/local store.png' },
+    { title: 'Redeem-Pay back points', url: '/redeem', image:'../assets/icon/redeem.png' },
+    { title: 'Reedem-MGM Points', url: '/redeemmgmpoints', image:'../assets/icon/mobiwik.png' },
+    { title: 'Wallet-Transfer', url: '/wallet', image:'../assets/icon/wallet.png' },
 
   ];
   public labels = ['Mutual Funds', 'Insurance', 'E-Gold', 'Express Fixed Deposit', 'Axis Plus Credit Card', 'Accepet Payments'];
